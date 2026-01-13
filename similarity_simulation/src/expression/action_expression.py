d1ca30de40e0717e563205b4a9a798f80c24a666
lucasvieira123 <lucas.vieira@aluno.uece.br>
2026-01-13 01:49:33 -0300

    delete arquivos desnecessarios

--- similarity_simulation/src/expression/action_expression.py
+++
@@ -1,6 +0,0 @@
-from expression.expression import Expression
-
-
-class ActionExpression(Expression):
-    def __init__(self, expression: str) -> None:
-            super().__init__(expression)
--- similarity_simulation/src/expression/conditional_expression.py
+++
@@ -1,5 +0,0 @@
-from expression.expression import Expression
-
-class ConditionalExpression(Expression):
-    def __init__(self, expression: str) -> None:
-        super().__init__(expression)
